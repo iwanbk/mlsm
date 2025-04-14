4e2fec01e3d345a267472b5dc6a79933cdeb8a5c
Iwan BK <iwanbk@gmail.com>
2025-04-14 16:26:16 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/iwanbk/mlsm
+
+go 1.23.1
+
+require github.com/huandu/skiplist v1.2.1
---
+++ mem_table.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/huandu/skiplist"
+)
+
+// basic LSM memtable using skiplist
+type MemTable struct {
+	kv skiplist.SkipList
+}
+
+func NewMemTable() *MemTable {
+	return &MemTable{
+		kv: *skiplist.New(skiplist.Byte),
+	}
+}
+
+func (m *MemTable) Put(key, value []byte) error {
+	// Insert the key-value pair into the skiplist
+	m.kv.Set(key, value)
+	return nil
+}
+
+func (m *MemTable) Get(key []byte) ([]byte, bool) {
+	// Get the value for the key from the skiplist
+	v, ok := m.kv.GetValue(key)
+	if !ok {
+		return nil, false
+	}
+	return v.([]byte), true
+}
