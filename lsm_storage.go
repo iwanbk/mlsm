1c5e907d19de1e88e8bb69cd6ae7aed92c64c6e4
Iwan BK <iwanbk@gmail.com>
2025-04-20 11:11:15 +0700

    freeze memtable

--- lsm_storage.go
+++ lsm_storage.go
@@ -1,13 +1,20 @@
 package main
 
-type LsmStorageState struct {
-	// current memtable
-	memtable *MemTable
-}
+import (
+	"sync"
+)
 
 type LsmStorage struct {
-	state *LsmStorageState
-	opt   *LsmStorageOption
+	// memtable Lock
+	mtLock sync.RWMutex
+	// current memtable
+	memtable *MemTable
+
+	// immutable memtables
+	// from latest to earliest
+	immMemtables []*MemTable
+
+	opt *LsmStorageOption
 }
 
 type LsmStorageOption struct {
@@ -18,25 +25,59 @@ type LsmStorageOption struct {
 
 func NewLsmStorage(opt *LsmStorageOption) *LsmStorage {
 	return &LsmStorage{
-		state: &LsmStorageState{
-			memtable: NewMemTable(),
-		},
-		opt: opt,
+		memtable:     NewMemTable(),
+		immMemtables: make([]*MemTable, 0),
+		opt:          opt,
 	}
 }
 
+// Put inserts the key-value pair into the memtable
 func (l *LsmStorage) Put(key, value []byte) error {
-	// Insert the key-value pair into the memtable
-	return l.state.memtable.Put(key, value)
-}
-func (l *LsmStorage) Get(key []byte) ([]byte, bool) {
-	// Get the value for the key from the memtable
-	return l.state.memtable.Get(key)
+	approxLen, err := func() (uint64, error) {
+		l.mtLock.RLock()
+		defer l.mtLock.RUnlock()
+
+		err := l.memtable.Put(key, value)
+		approxLen := l.memtable.ApproxLen()
+
+		return approxLen, err
+	}()
+	if err != nil {
+		return err
+	}
+	if approxLen >= uint64(l.opt.memtableSizeLimit) {
+		l.tryFreezeMemtable()
+	}
+	return nil
 }
 
+// Get the value for the key from the memtable
+func (l *LsmStorage) Get(key []byte) ([]byte, bool) {
+	l.mtLock.RLock()
+	defer l.mtLock.RUnlock()
+	return l.memtable.Get(key)
+}
+
+// Delete the key-value pair from the memtable
 func (l *LsmStorage) Delete(key []byte) error {
-	// Delete the key-value pair from the memtable
-	return l.state.memtable.Put(key, nil)
+	return l.Put(key, nil)
+}
+
+// Freeze the current memtable and create a new one
+func (l *LsmStorage) tryFreezeMemtable() {
+	newMemtable := NewMemTable()
+
+	l.mtLock.Lock()
+	defer l.mtLock.Unlock()
+
+	// check if the current memtable is already frozen
+	// by another goroutine
+	if l.memtable.ApproxLen() < uint64(l.opt.memtableSizeLimit) {
+		return
+	}
+
+	l.immMemtables = append(l.immMemtables, l.memtable)
+	l.memtable = newMemtable
 }
 
 type MiniLSM struct {
--- mem_table.go
+++ mem_table.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"sync/atomic"
+
 	"github.com/huandu/skiplist"
 )
 
 // basic LSM memtable using skiplist
 type MemTable struct {
-	kv skiplist.SkipList
+	kv        skiplist.SkipList
+	approxLen atomic.Uint64
 }
 
 func NewMemTable() *MemTable {
@@ -17,6 +20,10 @@ func NewMemTable() *MemTable {
 
 func (m *MemTable) Put(key, value []byte) error {
 	// Insert the key-value pair into the skiplist
+	if len(value) > 0 {
+		m.approxLen.Add(uint64(len(key) + len(value)))
+	}
+
 	m.kv.Set(key, value)
 	return nil
 }
@@ -28,4 +35,8 @@ func (m *MemTable) Get(key []byte) ([]byte, bool) {
 		return nil, false
 	}
 	return v.([]byte), true
+}
+
+func (m *MemTable) ApproxLen() uint64 {
+	return m.approxLen.Load()
 }
